ff32921aa8080aaac7357e9f6aceecb693f74cad
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-12-11 22:04:59 +0800

    Refactor PixelPacker to improve structure and functionality

--- src/cn/imaginary/toolkit/PixelPacker.java
+++ src/cn/imaginary/toolkit/PixelPacker.java
@@ -1,95 +1,151 @@
-package cn.imaginary.toolkit.image;
+package cn.imaginary.toolkit;
 
-//import cn.imaginary.toolkit.image.sheet.PixelScanner;
+import cn.imaginary.toolkit.image.PixelSheet;
+import cn.imaginary.toolkit.json.JsonObject;
 
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.awt.Point;
-import java.awt.Rectangle;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Iterator;
-
-//import javaev.awt.PolygonUtils;
-
+import java.util.Properties;
+import java.util.Set;
 import javax.imageio.ImageIO;
 
 public class PixelPacker {
+    public static int Line_MaxSize = -1;
+    public static int Row_MaxSize = -1;
+    public static int Width_KeepSize = 0;
+    public static int Height_KeepSize = 0;
+    public static int Width_MaxSize = -1;
+    public static int Height_MaxSize = -1;
 
+    private String lineSeparator = System.lineSeparator();
     private String suffix_Pack = "_pack_";
     private String suffix_Unpack = "_unpack_";
-    private String suffix_Png = "png";
+    private String suffix_Json = ".json";
+    private String suffix_Png = ".png";
+    private String suffix_Xml = ".xml";
+    private String tag_comment = "sprite pack";
+    private String tag_encoding = "utf-8";
+    private String tag_png = "png";
+    private String tag_polygon = "polygon";
 
-    public BufferedImage drawImage(BufferedImage[] array, int x, int y, int width, int height, int lineSize, int rowSize, int lineWidth, int rowHeight) {
-        if (null != array) {
-            if (lineSize <= 0) {
-                lineSize = array.length;
-            }
-            if (rowSize <= 0) {
-                rowSize = array.length / lineSize;
-            }
-            Rectangle rectangle = getBounds(array, width, height, lineSize, rowSize, lineWidth, rowHeight);
-//            System.out.println("rectangle:" + rectangle.toString());
-            BufferedImage image = new BufferedImage(rectangle.width, rectangle.height, BufferedImage.TYPE_INT_ARGB);
-            drawImage(image, array, width, height, lineSize, rowSize, lineWidth, rowHeight);
-            if (x != 0 || y != 0) {
-                return drawImage(image, x, y);
-            } else {
-                return image;
-            }
-        }
-        return null;
+    private PixelSheet pixelSheet = new PixelSheet();
+
+    private String readString(String filePath) {
+        return readString(new File(filePath));
     }
 
-    public void drawImage(BufferedImage image, BufferedImage[] array, int width, int height, int lineSize, int rowSize, int lineWidth, int rowHeight) {
-        if (null != image && null != array) {
-            int[] bounds = getBounds(array);
-//            System.out.println("bounds:" + bounds);
-            int w_max = bounds[1];
-            int h_max = bounds[3];
-            if (width <= 0) {
-                width = w_max;
-            }
-            if (height <= 0) {
-                height = h_max;
-            }
-//            System.out.println("width:" + width + "/height:" + height);
-            Graphics2D graphics2D = image.createGraphics();
-            for (int i = 0; i < array.length; i++) {
-                int line = i % lineSize;
-                int row = i / lineSize;
-                if (row >= rowSize) {
-                    break;
+    private String readString(File file) {
+        if (null != file) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String string;
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((string = reader.readLine()) != null) {
+                    stringBuilder.append(string);
+                    stringBuilder.append(lineSeparator);
                 }
-                int x = line * (width + lineWidth);
-                int y = row * (height + rowHeight);
-//                System.out.println("x:" + x + "/y:" + y);
-                drawImage(graphics2D, array[i], x, y, width, height);
+                reader.close();
+                string = stringBuilder.toString();
+                return string;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-            graphics2D.dispose();
-        }
-    }
-
-    private BufferedImage drawImage(BufferedImage image, int x, int y) {
-        if (null != image) {
-            BufferedImage root = new BufferedImage(image.getWidth() + x, image.getHeight() + y, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D graphics2D = root.createGraphics();
-            graphics2D.drawImage(image, x, y, null);
-            graphics2D.dispose();
-            return root;
         }
         return null;
     }
 
-    private void drawImage(Graphics2D graphics2D, BufferedImage image, int x, int y, int width, int height) {
-        if (null != graphics2D && null != image) {
-            if (width > 0 && height > 0) {
-                int w = image.getWidth();
-                int h = image.getHeight();
-                int x_ = (width - w) / 2;
-                int y_ = (height - h) / 2;
-                x += x_;
+    public Properties readProperties(File file) {
+        return readJson(file);
+//        return readXML(file);
+    }
+
+    private Properties readJson(File file) {
+        String string = readString(file);
+        JsonUtils jsonUtils = new JsonUtils();
+        JsonObject jsonObject = jsonUtils.parseJsonObject(string);
+        return toProperties(jsonObject);
+    }
+
+    private Properties readXML(File file) {
+        Properties properties = new Properties();
+        try {
+            properties.loadFromXML(Files.newInputStream(file.toPath()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties;
+    }
+
+    private Properties toProperties(JsonObject jsonObject) {
+        Properties properties = null;
+        if (null != jsonObject) {
+            properties = new Properties();
+            Properties prop = jsonObject.get();
+            Set<Object> kset = prop.keySet();
+            for (Iterator<Object> iterator = kset.iterator(); iterator.hasNext(); ) {
+                Object key = iterator.next();
+                Object value = prop.get(key);
+                if (value instanceof JsonObject) {
+                    properties.put(key.toString(), toProperties((JsonObject) value));
+                } else if (value instanceof Properties) {
+                    properties.put(key.toString(), value);
+                } else {
+                    properties.put(key.toString(), value.toString());
+                }
+            }
+        }
+        return properties;
+    }
+
+    private void writeString(String string, File file) {
+        if (null != string) {
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+                writer.write(string);
+                writer.flush();
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void writeProperties(Properties properties, File file) {
+//        System.out.println("properties: " + properties);
+        writeJson(properties, file);
+//        writeXML(properties, file);
+    }
+
+    private void writeJson(Properties properties, File file) {
+        if (null != properties) {
+            JsonObject jsonObject = toJsonObject(properties);
+            String info = jsonObject.toString();
+            writeString(info, file);
+        }
+    }
+
+    private void writeXML(Properties properties, File file) {
+        if (null != properties && null != file) {
+            try {
+                properties.storeToXML(Files.newOutputStream(file.toPath()), tag_comment, tag_encoding);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private JsonObject toJsonObject(Properties properties) {
+        JsonObject jsonObject = null;
+        if (null != properties) {
+            jsonObj                x += x_;
                 y += y_;
                 graphics2D.drawImage(image, x, y, null);
             }
